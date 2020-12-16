b4bb91f964003dc0c5e88bdf2ea7c528abb19f4f
Shawan Mandal <70664409+imshawan@users.noreply.github.com>
2020-12-16 18:50:28 +0530

    Track parcels in bulk, check screenshots!

---
+++ track-IN-Bulk/ekart.py
@@ -0,0 +1,62 @@
+import requests
+import pandas as pd
+import trackfile
+from bs4 import BeautifulSoup
+
+def track(trackingID):
+    rows1 = []
+    rows = []
+    r = requests.get(f'https://ekartlogistics.com/track/{trackingID}/').text
+
+    soup = BeautifulSoup(r, 'html.parser')
+    table1 = soup.find( "table", { 'class' : 'col-md-12 table-bordered table-striped table-condensed cf width-100' } )
+    table = soup.findAll( "table", { 'class' : 'col-md-12 table-bordered table-striped table-condensed cf width-100' } )
+
+
+    def rowgetDataText(tr, coltag='td'): # td (data) or th (header)       
+        return [td.get_text(strip=True) for td in tr.find_all(coltag)]
+    try:
+        trsf = table1.find_all('tr')
+    except AttributeError:
+        print("Invalid Tracking ID")
+        return
+    headerow1 = rowgetDataText(trsf[0], 'th')
+    if headerow1: # if there is a header row include first
+        rows1.append(headerow1)
+        trsf = trsf[1:]
+    for tr in trsf: # for every table row
+        rows1.append(rowgetDataText(tr, 'td') ) # data row       
+    headerow1 = rowgetDataText(trsf[0], 'th')
+    dftable1 = pd.DataFrame(rows1[1:], columns=rows1[0])
+    print(dftable1)
+
+
+
+    print("\n                             Detailed Tracking Details:")
+    for tables in table:
+        trs1 = tables.findAll('tr')
+    trs = trs1
+    headerow = rowgetDataText(trs[0], 'th')
+    if headerow: # if there is a header row include first
+        rows.append(headerow)
+        trs = trs[1:]
+    for tr in trs: # for every table row
+        rows.append(rowgetDataText(tr, 'td') ) # data row       
+    headerow = rowgetDataText(trs[0], 'th')
+
+    dftable = pd.DataFrame(rows[1:], columns=rows[0])
+
+    print("    " + dftable)
+    print("\n    *********************************************************************************************")
+
+    
+i=0
+tracking = trackfile.trackingIDS
+info = trackfile.item_info['iteminfo']
+for trackinginfo in tracking.values():
+    for ids in trackinginfo:
+        print("\nCurrently Tracking Item: %s \n" %info[i])
+        i +=1
+        track(ids)
+
+input("")
---
+++ track-IN-Bulk/trackfile.py
@@ -0,0 +1,6 @@
+trackingIDS = {
+    'trackingid':['FMPC1002781735','FMPC1001797855']
+    }  
+item_info = {
+    'iteminfo':['Book, VIBRANT HUES','Anti Hairfall Cleanser Shampoo Men & Women']
+}
